d5b25b06d0a21ceb01723a853acc0adf6110d145
suraj <surajkumarsahani1997@gmail.com>
2021-10-11 16:19:17 +0530

    some changes

--- app/src/main/java/com/martialcoder/moviebuff/MainActivity.kt
+++ app/src/main/java/com/martialcoder/moviebuff/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.martialcoder.moviebuff
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val SELECTED_MOVIE_KEY = "MovieSelected";
+        const val SELECTED_NAVIGATION_ITEM_KEY = "SelectedNavigationItem";
+    }
+
+    @BindView(R.id.drawer_layout)
+    var drawerLayout: DrawerLayout? = null
+    @BindView(R.id.navigation_view)
+    var navigationView: NavigationView? = null
+    var coordinatorLayout: CoordinatorLayout? = null
+
+    @BindView(R.id.toolbar)
+    var toolbar: Toolbar? = null
+
+//    @Nullable
+//    @BindView(R.id.movie_detail_container)
+//    var movieDetailContainer: ScrollView? = null
+//
+//    @Nullable
+//    @BindView(R.id.fab)
+//    var fab: FloatingActionButton? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    ButterKnife.bind(this)
+
     }
 }
---
+++ app/src/main/java/com/martialcoder/moviebuff/PopularMoviesApp.java
@@ -0,0 +1,8 @@
+package com.martialcoder.moviebuff;
+
+import android.app.Application;
+
+public class PopularMoviesApp extends Application {
+
+
+}
